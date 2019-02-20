4aa469dbaf1a00e7b6d3549084544cc84bf6fac9
Joan Lyness <joanrigdon@gmail.com>
2019-02-20 00:49:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,6 +8,8 @@ class Song
   @@count = 0
 
   def initialize(name, artist, genre)
+    @@count += 1
+
     @name = name
     @@count += 1
 
@@ -35,8 +37,8 @@ class Song
     #@@genres = [pop, rap, rock, rap, pop]
     #=> {pop => 2, rap => 2, rock => 1}
     @@genres.each do |genre|
-      if genre_count.keys.include?(genre)
-        genre_count[genre] = genre_count[genre]+1
+      if genre_count.has_key?(genre)
+        genre_count[genre] += 1
       else
         genre_count[genre] = 1
     end

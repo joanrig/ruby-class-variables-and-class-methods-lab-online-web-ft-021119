5e09fc35a274f83b62e17470db7facdd25607fa9
Joan Lyness <joanrigdon@gmail.com>
2019-02-20 00:55:33 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,11 +37,10 @@ class Song
     #@@genres = [pop, rap, rock, rap, pop]
     #=> {pop => 2, rap => 2, rock => 1}
     @@genres.each do |genre|
-      if genre_count.includes?(genre)
+      if genre_count.has_key?(genre)
         genre_count[genre] += 1
       else
         genre_count[genre] = 1
-      end
     end
     binding.pry
     genre_count

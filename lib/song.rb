986dbc03610df970b545f12af45613741f7da2b0
Joan Lyness <joanrigdon@gmail.com>
2019-02-20 01:30:31 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,7 +32,7 @@ class Song
 
   def self.genre_count
     genre_count = {}
-    #@@genres = [pop, rap, rock, rap, pop]
+    #@@genres = ["pop", "rap", "rock", "rap", "pop"]
     #=> {pop => 2, rap => 2, rock => 1}
     @@genres.each do |genre|
       if genre_count[genre]
@@ -42,6 +42,21 @@ class Song
       end
     end
     genre_count
+    #binding.pry
+  end
+
+  def self.artist_count
+    artist_count = {}
+    #@@artists = ["Beatles", "Jay-Z", "Grateful Dead", "Jay=Z"]
+    #=> {"Jay-Z" => 2, "Beatles"=> 1, "Grateful Dead" => 1}
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1
+      else
+        artist_count[artist] = 1
+      end
+    end
+    artist_count
     #binding.pry
   end
 

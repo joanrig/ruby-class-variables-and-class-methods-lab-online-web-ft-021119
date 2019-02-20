2b6926f88f0b4e91d3a36509b9fb37a6c604733d
Joan Lyness <joanrigdon@gmail.com>
2019-02-20 00:16:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,26 +1,25 @@
 class Song
-  attr_accessor
   attr_reader :artist, :genre, :name
 
-  @@count = 0
+  #@@count = 0
   @@artists = []
   @@genres_hash = {}
 
   def initialize(name, artist, genre)
     @name = name
-    @@count += 1
+    #@@count += 1
 
     @artist = artist
     @@artists<< @artist
 
     @genre = genre
-    @genres_array << @genre
+    #@genres_array << @genre
 
-    if @@genres_hash.keys.include?(@genre)
-      @@genres_hash[@genre] = @name
-    else
-      @@genres_hash<< {:@genre => @name)
-    end
+    # if @@genres_hash.keys.include?(@genre)
+    #   @@genres_hash[@genre] = @name
+    # else
+    #   @@genres_hash<< {:@genre => @name)
+    # end
   end
 
 
